d5b4ab3ccded66bead2c9981eb50f15e10633bf4
realsora <realsora@gmail.com>
2019-12-16 14:36:01 +0000

    lesson11

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -15,11 +15,7 @@ end
     end
 
     it "should successfully show the new form" do
-      user = User.create(
-        email: 'fakeuser@gmail.com', 
-        password: 'secretPassword', 
-        password_confirmation: 'secretPassword'
-        )
+      user = FactoryBot.create(:user)
       sign_in user
 
       get :new
@@ -35,11 +31,7 @@ end
     end
 
     it "should successfully create a new gram in our database" do
-      user = User.create (
-        email: 'fakeuser@gmail.com', 
-        password: 'secretPassword', 
-        password_confirmation: 'secretPassword'
-        )
+      user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: { gram: {message: 'Hello!'}}
@@ -51,11 +43,7 @@ end
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email: 'fakeuser@gmail.com', 
-        password: 'secretPassword', 
-        password_confirmation: 'secretPassword'
-        )
+      user = FactoryBot.create(:user)
       sign_in user
 
       gram_count= Gram.count
